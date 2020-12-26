16927234e8b41c2630d7fe2f3bc5f3d3a72953f3
vishvapatel2529 <email_2529vishva@gmail.com>
2020-12-26 16:15:43 -0600

    project done.

--- hello.js
+++ hello.js
@@ -1,12 +1,19 @@
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
-  let billAmount = document.getElementById("billamount").value;
+  let billAmount = Number(document.getElementById("billamount").value);
   let tippercentage = document.getElementById("Percentage").value;
 
-  const tipamount = (document.getElementById("tipamount").value =
-    (billAmount * tippercentage) / 100);
+  console.log(billAmount, tippercentage);
 
-  document.getElementById("totalbill").value =
-    parseFloat(billAmount) + parseFloat(tipamount);
+  const tipamount = (document.getElementById("tipamount").value =
+    (billAmount * tippercentage) / 100).toFixed(3);
+
+  // console.log(billAmount, tippercentage);
+  console.log(tipamount);
+
+  const totalvalue = (document.getElementById("totalbill").value =
+    parseFloat(billAmount) + parseFloat(tipamount));
+
+  console.log(totalvalue);
 });
